cc68b2e40bdad07e12b01f23f7a8a121e8a682ed
vjaydehade <126457221+vjaydehade@users.noreply.github.com>
2023-02-28 14:06:02 +0530

    Add files via upload

---
+++ MiniProject/src/Calculator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MiniProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SHIV
+ *
+ */
+module MiniProject {
+}
